dfd393b8cb0f9920137553324fe2844f601bdb08
Lee Chang Ha <30414979+lchbest10@users.noreply.github.com>
2019-10-05 03:20:58 +0900

    ui수정 및 Splash Activity 추가

---
+++ app/src/main/java/com/example/mobilekiosk/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobilekiosk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.mobilekiosk.R;
+import com.example.mobilekiosk.MainActivity;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
