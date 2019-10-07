2b85dbbb666988f23e226e1e4ca0165f9c767b1e
kiaeh2323 <kiaeh2323@naver.com>
2019-10-07 18:07:01 +0900

    이미지 추가 및 MainActivity 완료

--- app/src/main/java/com/example/mobilekiosk/MainActivity.java
+++ app/src/main/java/com/example/mobilekiosk/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView map;
-
+    ImageButton btn_bobburger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         map = (ImageView)findViewById(R.id.map);
+        btn_bobburger = (ImageButton)findViewById(R.id.btn_bobburger);
 
-
-
+        btn_bobburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,OrderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
