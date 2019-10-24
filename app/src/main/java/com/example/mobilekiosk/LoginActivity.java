32a476583e6f2734924af9e29c11a61a459ca2ce
Park Dong Ho <padoho987@gmail.com>
2019-10-24 17:07:38 +0900

    파이어베이스 주문 연동틀 완성

--- app/src/main/java/com/example/mobilekiosk/LoginActivity.java
+++ app/src/main/java/com/example/mobilekiosk/LoginActivity.java
@@ -76,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                 LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 queue.add(loginRequest);
+                
             }
         });
 
